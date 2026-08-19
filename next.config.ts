fd1c10f739a4571f4156c92828643a7e520580e1
nudesign95 <designmasterprint06@gmail.com>
2026-08-19 12:35:56 -0400

    fix: actualizacion de next.config.ts para eliminar clave eslint obsoleta

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Nombre correcto para ignorar errores de TS en el build
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
